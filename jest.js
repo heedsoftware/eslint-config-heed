9a167092aecf738ef508b7678b92813cc6689384
tomfin46 <tomfin46@gmail.com>
2019-11-06 10:48:46 +0000

    feat(jest): Rework jest config to account for extends support in eslint overrides
    BREAKING CHANGE: Jest config now disables global-require rule and enables jest globals for all files
    when extended

--- jest.js
+++ jest.js
@@ -9,31 +9,13 @@ module.exports = {
     'plugin:jest-formatting/recommended'
   ],
   env: {
-    'jest/globals': false
+    'jest/globals': true
   },
   rules: {
     // Allow padding before beforeAll and beforeEach blocks as we often declare variables there to be initialised within the block
     'jest-formatting/padding-around-before-all-blocks': 'off',
-    'jest-formatting/padding-around-before-each-blocks': 'off'
-  },
-  overrides: [
-    {
-      files: [
-        'setupTests.js',
-        'test-setup-*.browser.js',
-        '**/*.spec.js',
-        '**/__mocks__/**',
-        '**/__test_helpers__/**',
-        '**/__tests__/**',
-        '**/__TESTS__/**'
-      ],
-      env: {
-        'jest/globals': true
-      },
-      rules: {
-        // Allow inline require() when using doMock conventions
-        'global-require': 'off',
-      }
-    }
-  ]
+    'jest-formatting/padding-around-before-each-blocks': 'off',
+    // Allow inline require() when using doMock conventions
+    'global-require': 'off',
+  }
 }
